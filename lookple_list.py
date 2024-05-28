218d1c5b98b19ca1104b68a78440cafb33ed18f2
Hwanghuntae <ghkdgnsxo2484@naver.com>
2024-05-28 20:54:39 +0900

    예외처리

--- lookple_list.py
+++ lookple_list.py
@@ -55,28 +55,50 @@ def save_size(name_list, size_list):
   return "complete save data"
 
 print(get_list(main_url))
-print(get_size(link_list[2], name_list[2]))
+print(get_size(link_list[7], name_list[7]))
 print(size_dic_list)
 
 import re
 def parse_description(description):
-    clean_description = re.sub(r'[^0-9가-힣\s]', '', description)
-    parts = re.split(r'\s+', clean_description)
-    name = parts[0]
-    measurements = parts[1:]
-    if '어깨' in description:
-        size_values = [measurements[1], measurements[3], None, measurements[5], measurements[7], measurements[9]]
+    size_names = ['FREE', 'S', 'M', 'L', 'XL', 'XLL']
+    name_match = re.match(r'^(FREE|S|M|L|XL|XLL)\s*:', description)
+    if name_match:
+        name = name_match.group(1)
+        measurements = description[len(name_match.group(0)):].strip()
     else:
-        size_values = [measurements[1], measurements[3], None, measurements[5], measurements[7], measurements[9]]
+        name = ""
+        measurements = description
+
+    measurement_keywords = ['어깨', '가슴', '소매', '암홀', '총장', '허리', '밑위', '허벅지', '밑단']
+    clean_measurements = re.split(r'\s+', measurements)
     
-    return [name] + size_values
+    size_values = [None] * 6  
+
+    keyword_indices = {'어깨': 0, '가슴': 1, '소매': 3, '암홀': 4, '총장': 5, '허리': 6, '밑위': 7, '허벅지': 8, '밑단': 9}
+
+    current_index = 0
+    while current_index < len(clean_measurements):
+        keyword = clean_measurements[current_index]
+        if keyword in keyword_indices:
+            index = keyword_indices[keyword]
+            if index < len(size_values):
+                size_values[index] = clean_measurements[current_index + 1]
+            current_index += 2
+        else:
+            current_index += 1
+
+    return [name] + size_values[:6]
+
+def split_descriptions(text):
+    pattern = r'(?<=\d)(?=[가-힣]+\s*:|[A-Z]+\s*:)'
+    return re.split(pattern, text)
 
 converted_size_dic_list = []
 
 for item in size_dic_list:
     new_item = {}
     for key, value in item.items():
-        descriptions = re.split(r'(?<=\d)(?=[가-힣]+\s*:)', value[0])
+        descriptions = split_descriptions(value[0])
         new_value = [parse_description(desc.strip()) for desc in descriptions]
         new_item[key] = new_value
     converted_size_dic_list.append(new_item)
@@ -84,9 +106,7 @@ for item in size_dic_list:
 print(converted_size_dic_list)
 
 
-
-
-# DB저장을 위한 데이터 정제
+# 예외처리 추가
 
 
 
