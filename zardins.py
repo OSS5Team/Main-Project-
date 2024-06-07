ae2b940eb27a4a31dad09fffe2c29230cc355da4
Hwanghuntae <ghkdgnsxo2484@naver.com>
2024-06-08 00:34:38 +0900

    make iteration

--- zardins.py
+++ zardins.py
@@ -18,6 +18,9 @@ list_page = soup.select(".prdList.grid3")
 
 for li in list_page:
   divs = li.find_all(attrs={'class': "thumbnail"})
+  for div in divs:
+
+
 td_tags = soup.find_all("td")
 for td in td_tags:
   if 'style' in td.attrs:
