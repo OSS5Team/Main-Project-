cdc133b1cb5fd94178eb82ca3aa2b602d6d3ef47
Hwanghuntae <ghkdgnsxo2484@naver.com>
2024-06-08 00:11:36 +0900

    declare lists

--- zardins.py
+++ zardins.py
@@ -7,3 +7,6 @@ main_url = "https://www.zardins.com/product/list.html?cate_no=43"
 
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
+
+name_list, img_list, link_list, size_dic_list = [], [], [], []
+
