dddd048a840ac048000821e13cd3b8f26e04839b
Hwanghuntae <ghkdgnsxo2484@naver.com>
2024-06-08 00:41:53 +0900

    declare function

--- zardins.py
+++ zardins.py
@@ -9,21 +9,21 @@ chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
 
 name_list, img_list, link_list, size_dic_list, size_list, size_list = [], [], [], [], []
+def get_list(url):
+  driver.get(url)
+  driver.implicitly_wait(time_to_wait=5)
+  page = driver.page_source
+  soup = BeautifulSoup(page, "html.parser")
+  list_page = soup.select(".prdList.grid3")
 
-driver.get(url)
-driver.implicitly_wait(time_to_wait=5)
-page = driver.page_source
-soup = BeautifulSoup(page, "html.parser")
-list_page = soup.select(".prdList.grid3")
-
-for li in list_page:
-  divs = li.find_all(attrs={'class': "thumbnail"})
-  for div in divs:
-    prd_div = div.find('div', class_='prdImg')
-    if prd_div:
-      link_list.append("https://www.byslim.com/" + prd_div.find('a')['href'])
-      name_list.append(prd_div.find('img')['alt'])
-      img_list.append(prd_div.find('img')['src'])
+  for li in list_page:
+    divs = li.find_all(attrs={'class': "thumbnail"})
+    for div in divs:
+      prd_div = div.find('div', class_='prdImg')
+      if prd_div:
+        link_list.append("https://www.byslim.com/" + prd_div.find('a')['href'])
+        name_list.append(prd_div.find('img')['alt'])
+        img_list.append(prd_div.find('img')['src'])
 
 td_tags = soup.find_all("td")
 for td in td_tags:
