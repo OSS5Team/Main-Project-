cfb4f960bbeb8a0ab68b9374d8c2dbc6346ff77d
JW3154 <70872664+JW3154@users.noreply.github.com>
2024-06-09 19:19:23 +0900

    Update okkane_data.py

--- okkane_data.py
+++ okkane_data.py
@@ -3,3 +3,7 @@ from bs4 import BeautifulSoup
 
 # URL 가져오기
 url = "https://www.okkane.co.kr/shop/shopbrand.html?xcode=073&type=Y"
+
+# URL에서 HTML 가져오기
+response = requests.get(url)
+html = response.content
